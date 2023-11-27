9e04ba8604ce34b15e5ac761879e5b41a25efd02
Jasur <beretax308@gmail.com>
2023-11-27 23:40:09 +0500

    process

---
+++ client/src/app/auth/[..nextauth]/route.ts
@@ -0,0 +1,5 @@
+import nextAuthLib from "@/lib/next-auth.lib";
+
+const handler = nextAuthLib;
+
+export { handler as GET, handler as POST };
--- client/src/lib/next-auth.lib.ts
+++ client/src/lib/next-auth.lib.ts
@@ -1,4 +1,6 @@
+import { $fetch } from "@/$api/api.fetch";
 import { IAuthFormState } from "@/components/screens/auth/auth.types";
+import { IUser } from "@/types/user.types";
 import NextAuth from "next-auth/next";
 import Credentials from "next-auth/providers/credentials";
 
@@ -20,8 +22,16 @@ const options = {
       async authorize(credentials) {
         const { email, password } = credentials as IAuthFormState;
 
+        const data = await $fetch.get<IUser[]>(
+          `/users?filters[email][$eq]=${email}`
+        );
+
+        console.log(data);
+
         return null;
       },
     }),
   ],
 };
+
+export default NextAuth(options);
